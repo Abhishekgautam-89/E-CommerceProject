53ca5e6537ea5ba1f7527004c5970bc138ed7554
Abhishekgautam-89 <abhishek.gautam89@outlook.com>
2022-11-22 16:03:26 +0530

    Cart-Layout Changed

--- Front-End/scripts/style.js
+++ Front-End/scripts/style.js
@@ -2,19 +2,22 @@
 const topCartBtn = document.getElementById('top-cart-btn');
 const bottomCartBtn = document.getElementById('bottom-cart-btn');
 const xButton = document.getElementById('close-btn');
+const cartContainer = document.getElementById('cart-container')
 
 // Button Event Listeners
-{
-    topCartBtn.addEventListener('click', () => {
-        nav.classList.toggle('active');
+
+    topCartBtn.addEventListener('click', () => {       
+        cartContainer.classList.toggle('active')        
     })
 
+
     bottomCartBtn.addEventListener('click', () => {
-        nav.classList.toggle('active');
+        cartContainer.classList.toggle('active')       
     })
 
     xButton.addEventListener('click', () => {
-        nav.classList.toggle('active');
+        cartContainer.classList.toggle('active');       
     })
-}
+
+
 
